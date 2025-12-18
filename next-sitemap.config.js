68f114f5c2c9dad3dc681c446d7344f6a6f47605
Hornshade <sabin.mihai@yahoo.com>
2025-12-18 12:30:28 +0200

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://bucovina-med.ro',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-}
+  siteUrl: 'https://bucovina-med.ro', // URL-ul principal al site-ului tău
+  generateRobotsTxt: true,            // generează automat robots.txt
+  sitemapSize: 5000,                   // numărul maxim de URL-uri pe sitemap
+  changefreq: 'daily',                 // valoare implicită pentru changefreq
+  priority: 0.7,                       // valoare implicită pentru priority
+  autoLastmod: true,                   // actualizează automat <lastmod> la ultima build
+  generateIndexSitemap: true,          // generează sitemap index dacă sunt mai multe sitemap-uri
+  exclude: ['/admin/**', '/secret/**'], // exclud URL-uri dacă e nevoie
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' }  // permite Google să indexeze tot site-ul
+    ],
+  },
+};
